a0c80b35ed1a3092a3ad837fd6cfbc4a1df9dc4b
CorentinMaille <81628097+CorentinMaille@users.noreply.github.com>
2023-11-03 14:31:54 +0100

    functional api baby (#7)
    Co-authored-by: Corentin Maille <maillecorentin@pm.me>

--- backend/data_service.py
+++ backend/data_service.py
@@ -1,19 +1,69 @@
 import pandas as pd
+import spacy
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
 
 class DataService:
     def __init__(self):
+        # Load Natural Language Processing english Model
+        self.nlp = spacy.load('en_core_web_sm')
+
         # Load CSV data
-        self.dataframe = self.load_csv()
+        self.dataframe = self.load_and_clean_csv()
 
-    def load_csv():
-        pd.read_csv()
+        # Train model
+        self.model = self.train_model()
 
-        return False
 
-    def is_in_db(comment):
+    def treat_comment(self, comment):
+        spacy_comment = self.nlp(comment, disable=["parser", "tagger", "ner", "textcat"])
+        treated_tokens = [w.text for w in spacy_comment if w.is_alpha and not w.is_stop]
+        return " ".join(treated_tokens)
+
+
+    def load_and_clean_csv(self):
+        # Read csv
+        dataframe = pd.read_csv('../data/train.csv')
+
+        # If comment.sentiment is True, then the comment is negative
+        dataframe['sentiment'] = dataframe[["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]].any(axis=1).astype(int)
+        dataframe = dataframe.drop(["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"], axis=1)
         
 
-        return False
+        # Reduce dataset to have same number of positive & negative comments
+        df_positive = dataframe[dataframe['sentiment'] == 1]
+        df_negative = dataframe[dataframe['sentiment'] == 0]
+        df_negative = df_negative.sample(n=len(dataframe[dataframe['sentiment'] == 1]), random_state=42)
+        dataframe = pd.concat([df_positive, df_negative])
+
+        # Clean all comments
+        dataframe['comment_text'] = dataframe['comment_text'].apply(self.treat_comment)
+
+        return dataframe
     
-    def verify_comment(comment):
-        return False
+
+    def train_model(self):
+        # Prepare training data
+        Y = self.dataframe["sentiment"]
+        self.vectorizer = TfidfVectorizer()
+        X = self.vectorizer.fit_transform(self.dataframe["comment_text"])
+        X, Y
+
+        # Train model
+        X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.25,random_state=42)
+        model = LogisticRegression()
+        model.fit(X_train, Y_train)
+        return model
+
+    
+    def verify_comment(self, comment):
+        clean_comment = self.treat_comment(comment)
+        encoded_comment = self.vectorizer.transform([clean_comment])
+        prediction = self.model.predict(encoded_comment)
+        return prediction[0]
+
+    
+    
