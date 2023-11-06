03b6459efad2bb63a83f954d42010f7a5ecce4a7
CorentinMaille <81628097+CorentinMaille@users.noreply.github.com>
2023-11-06 14:19:36 +0100

    omg commit de fou!!! (#12)
    Co-authored-by: Corentin Maille <maillecorentin@pm.me>

--- backend/data_service.py
+++ backend/data_service.py
@@ -13,6 +13,7 @@ model_path = '../data/model.joblib'
 vectorizer_path = '../data/vectorizer.joblib'
 
 class DataService:
+
     def __init__(self):
         # Load Natural Language Processing english Model
         self.nlp = spacy.load('en_core_web_sm')
@@ -28,35 +29,38 @@ class DataService:
         treated_tokens = [w.text for w in spacy_comment if (w.is_alpha and not w.is_stop) or w.text.lower() in ["not", "no", "never", "none", "nothing", "don't", "myself", "like"]]
         return " ".join(treated_tokens)
 
+    def load_and_clean_toxic(self):
+        # Read csv
+        toxic_df = pd.read_csv('../data/toxic.csv')
+
+        # Clean dataframe
+        toxic_df['status'] = toxic_df[["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]].any(axis=1).astype(int)
+        toxic_df = toxic_df.drop(["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"], axis=1)
+
+        # Reduce dataset
+        positive_df = toxic_df[toxic_df['status'] == 1]
+        negative_df = toxic_df[toxic_df['status'] == 0]
+        negative_df = negative_df.sample(n=len(toxic_df[toxic_df['status'] == 1]), random_state=42)
+
+        toxic_df = pd.concat([positive_df, negative_df])
+        toxic_df = toxic_df.reset_index()
+        return toxic_df
+
+    def load_and_clean_suicide(self):
+        suicide_df = pd.read_csv('../data/suicide.csv')
+        suicide_df.replace({'non-suicide': 0, 'suicide': 2},inplace=True)
+        suicide_df.rename(columns={'text': 'comment_text', 'class': 'status'}, inplace=True)
+        suicide_df.drop('Unnamed: 0', axis=1, inplace=True)
+        return suicide_df
 
 
-    def load_and_clean_csv(self):
-        # Read all csv
-        jigsaw = pd.read_csv('../data/jigsaw.csv')
-        train = pd.read_csv('../data/train.csv')
-        suicide = pd.read_csv('../data/suicide.csv')
-        
-        jigsaw_train = pd.concat([train, jigsaw])
+    def load_dataframe(self):
+        # load and clean all datasets
+        toxic_df = self.load_and_clean_toxic()
+        suicide_df = self.load_and_clean_suicide()
 
-        # Clean jigsaw & train
-        jigsaw_train['status'] = jigsaw_train[["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]].any(axis=1).astype(int)
-        jigsaw_train = jigsaw_train.drop(["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"], axis=1)
-
-        # Reduce dataset to have same number of positive & negative comments
-        df_positive = jigsaw_train[jigsaw_train['status'] == 1]
-        df_negative = jigsaw_train[jigsaw_train['status'] == 0]
-        df_negative = df_negative.sample(n=len(jigsaw_train[jigsaw_train['status'] == 1]), random_state=42)
-        
-        jigsaw_train = pd.concat([df_positive, df_negative])
-
-        jigsaw_train = jigsaw_train.reset_index()
-        jigsaw_train.drop(["index", "id"], axis=1, inplace=True)
-
-        suicide.replace({'non-suicide': 0, 'suicide': 2},inplace=True)
-        suicide.rename(columns={'text': 'comment_text', 'class': 'status'}, inplace=True)
-        suicide.drop('Unnamed: 0', axis=1, inplace=True)
-
-        dataframe = pd.concat([jigsaw_train, suicide])
+        # Merge datasets
+        dataframe = pd.concat([suicide_df, toxic_df])
         self.dataframe = dataframe.sample(frac=1).reset_index(drop=True) # Shuffle data
 
         # Clean all comments
@@ -85,32 +89,25 @@ class DataService:
 
     
     def verify_comment(self, comment):
-        print('CLEAN')
-
         # Clean comment
         clean_comment = self.treat_comment(comment)
 
-        print('VECTORIZER')
-
         # Load vectorizer
         self.vectorizer = joblib.load(vectorizer_path)
-
-        print('PREDICT')
 
         # Predict
         encoded_comment = self.vectorizer.transform([clean_comment])
         prediction = self.model.predict(encoded_comment)
 
-        print('GET STATUS LABEL')
-
         return self.get_status_label_from_code(prediction[0]);
+
 
     def load_model(self):
         if os.path.exists(model_path):    
             return joblib.load(model_path)
         else:
             # Load CSV data
-            self.dataframe = self.load_and_clean_csv()
+            self.dataframe = self.load_dataframe()
             
             # Train AI model
             model = self.train_model()
@@ -119,11 +116,8 @@ class DataService:
             joblib.dump(model, model_path)
             return model 
     
-    # Translate status code to status label
-    def get_status_label_from_code(self, status_code):
-        
-        print("status code : " + str(status_code))
 
+    def get_status_label_from_code(self, status_code):
         if status_code == 0:
             return 'positive'
         elif status_code == 1:
